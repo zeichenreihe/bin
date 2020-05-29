6b3cfd456cc63337abb058bfd390e5cedc9a738a
Johannes Schmatz <johannes_schmatz@gmx.de>
2020-05-29 22:37:33 +0200

    adding license header to pacman-licenses.sh

--- pacman-licenses.sh
+++ pacman-licenses.sh
@@ -1,1 +1,46 @@
-pacman -Si $(pacman -Qq) | grep Lizenzen | sort -u | sed 's/Lizenzen                 : //g ; s/:/ /g ; s/ /\n/g' | sort -u
+#!/bin/bash
+#    pacman-licenses.sh - list all licenses of installed software
+#    Copyright (C) 2020  Johannes Schmatz <johannes_schmatz@gmx.de>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# old german variant
+#pacman -Qi $(pacman -Qq) | \
+#	grep Lizenzen | \
+#	sort -u | \
+#	sed 's/Lizenzen                 : //g' | \
+#	sed 's/custom:/custom /g' | \
+#	sed 's/ /\n/g' | \
+#	sort -u
+
+# new german variant
+#pacman -Qi $(pacman -Qq) | \
+#	grep Lizenzen | \
+#	sed 's/Lizenzen                 : //g' | \
+#	sed 's/custom:/custom  /g' | \
+#	sed 's/  /\n/g; s/\n /\n/g' | \
+#	tr '[:upper:]' '[:lower:]' | \
+#	sort -u
+
+# international variant
+LC_ALL=C pacman -Qi $(pacman -Qq) | \
+	grep 'Licenses' | \
+	sed 's/Licenses        : //g' | \
+	sed 's/custom:/custom  /g' | \
+	sed 's/  /\n/g; s/\n /\n/g' | \
+	tr '[:upper:]' '[:lower:]' | \
+	sort -u
+
+# syntax
+# vim: ts=8 sts=8 sw=8 noet si syntax=bash
